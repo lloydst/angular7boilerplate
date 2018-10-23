bfa110fc8d99e02c1bf79d9cd41e43bdf49e81d8
Lloyd Stumpel <lloyd.stumpel@poort80.nl>
2018-10-23 12:47:22 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { ActiveComponent } from './active/active.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'lazy',
+        //component: AppComponent,
+        loadChildren: './lazy/lazy.module#LazyModule',
+    },
+    {
+        path: 'active',
+        //component: AppComponent,
+        children: [
+            { path: '', component:ActiveComponent}]
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ActiveModule } from './active/active.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ActiveModule, // active component comes out of here
+
   ],
   providers: [],
   bootstrap: [AppComponent]
